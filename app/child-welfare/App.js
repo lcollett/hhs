1dff9da090a751cc26f8b041b285f4cd793eff7a
brito <francisco.brito@gmail.com>
2014-12-01 07:46:42 -0800

    stuff

--- app/child-welfare/App.js
+++ app/child-welfare/App.js
@@ -1,11 +1,35 @@
-function App(data){
-  console.info(data);
-  Object.keys(this).map(function(name){
-    var value = this[name];
-    console.warn(name, value)
-  }, this);
-  return this; 
+/** App.call(this) */
+function App(){
+  // where is the feature index on the document?
+  var index = document.querySelectorAll('#index,nav')[0];
+  // initialize all features
+  for (var name in this.features){
+    var feature = this.features[name],
+        // create slug, href, url or id
+        href = '#' + name.toLocaleLowerCase()
+          .replace(/\s+/,'-');
+    
+    // a. features can link to a page
+    if ('string' == typeof feature)
+      href = feature;
+    // b. or created from configuration
+    if (feature instanceof Array)
+      feature.forEach(App.Feature.bind(name));
+    
+    // 0. generate nav or index
+      index
+      .insertAdjacentHTML('beforeend',
+                          // adding a local link
+                          name.link(href));
+    // 1. 
+  }
 }
-  
-if ('run')
-  App.call(config, data);
+/** [].forEach(App.Feature) */
+App.Feature = function(item){
+  // console.log(item);
+  // iterate through collection of allegedly similar things
+  for (var property in item){
+    // need feature name?
+    console.info(this, property, item[property]);
+  }
+};
--- app/child-welfare/config.js
+++ app/child-welfare/config.js
@@ -1,7 +1,27 @@
-/** App config */
-var config = {
+/** App.call({ name:'string', features:Array }) */
+App.call({
   name: 'Child Welfare',
-  features: 'Dashboard, Cases, Clients, Documents;' +
-            'Reports, Regulations, Contacts;' +
-            'About, Help, Feedback'
-};
+  features: {
+    Cases: [
+      {
+        due: new Date + (12*60*60*1e3)/* 12h later */,
+        id: 'UTAE0341422',
+        type: 'Home Visit',
+        priority: 'high'
+      }
+    ],
+    Children: [
+      {
+        id: 'UTAE0341422',
+        family: 'Sanchez', 
+        given: 'Jerry',
+        birthdate: new Date('1998-5-3'),
+        race: 'Hispanic/Latino',
+        school: 'Lincoln High',
+        placement: 'Jones Family',
+        img: 'data:image/gif;base64,R0lGODlhAQABAAAAACw=',
+      }
+    ],
+    About: 'page/about.html'
+  }
+});
